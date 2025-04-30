1943554591208b4e4459b0c83b10e47764ad6cc0
Triumph-light <2495685883@qq.com>
2025-04-30 15:07:46 +0800

    test: add antd test

--- examples/src/App.tsx
+++ examples/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button, Card, Space } from 'antd'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,14 +18,22 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <Card>
+        <Space>
+          <Button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </Button>
+          <Button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </Button>
+          <Button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </Button>
+        </Space>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
-      </div>
+      </Card>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
